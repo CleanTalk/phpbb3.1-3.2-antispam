5102c3e001807f71acb4c2304ecccaa29bfc0f95
AntonV1211 <antonV1211@yandex.ru>
2026-07-24 12:11:11 +0700

    Post-review note edits

--- cleantalk/antispam/lib/Cleantalk/Common/Variables/Server.php
+++ cleantalk/antispam/lib/Cleantalk/Common/Variables/Server.php
@@ -41,8 +41,8 @@ class Server extends ServerVariables
             $value = strtoupper($value);
         }
 
-        // Convert HTML chars for HTTP_USER_AGENT, HTTP_USER_AGENT, SERVER_NAME
-        if (in_array($name, array('HTTP_USER_AGENT', 'HTTP_USER_AGENT', 'SERVER_NAME'))) {
+        // Convert HTML chars for HTTP_USER_AGENT, SERVER_NAME
+        if (in_array($name, array('HTTP_USER_AGENT', 'SERVER_NAME'))) {
             $value = htmlspecialchars($value);
         }
 
